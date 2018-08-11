6f51df3b39ae953205967575017cf63ab9118f45
alaamarneh <ala.mamoon@gmail.com>
2018-08-11 20:16:27 +0300

    update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
 #some text
+import unittest
+import json
+
+from main import app
+
+class TestURLShorten(unittest.TestCase):
+    client = None
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.client = app.test_client()
+
+    def test_get(self):
+        rv = self.client.get("/?arg1=2&arg2=3&op=add")
+        json = rv.get_json()
+        print (json['result'])
+        self.assertTrue(float(json['result']) == 5)
+        self.assertTrue(rv.status_code == 200)
+
+if __name__ == '__main__':
+    unittest.main()
